a3414ae8ead5162f95e10cc05102a4fa080ff33c
Kevin Cazelles <kevin.cazelles@gmail.com>
2022-06-10 10:47:26 -0400

    Initial commit

---
+++ root_path.R
@@ -0,0 +1,13 @@
+root_path<-function(){
+  #laptop
+  #root = "C:/Users/Elise/Dropbox/TELUQ/ENSEIGNEMENT/Cours/AnalyseSpatiale/Developpement"
+  
+  #Maison
+  root = "E:/ELF/Dropbox/TELUQ/ENSEIGNEMENT/Cours/AnalyseSpatiale/Developpement"
+  
+  #TELUQ
+  #root = "D:/Dropbox/TELUQ/ENSEIGNEMENT/Cours/AnalyseSpatiale/Developpement"
+  
+  return(root)
+  
+}
